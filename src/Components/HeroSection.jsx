d2c50d13521a3b3b9db58315dbffbd396258afc7
sabbirRashed <srsabbirrahman12@gmail.com>
2026-08-09 23:51:37 +0600

    set up project folder structure

---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <div className='border min-h-[500px]'>
+            
+        </div>
+    );
+};
+
+export default HeroSection;
